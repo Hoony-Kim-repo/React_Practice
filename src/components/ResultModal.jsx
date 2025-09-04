0405ad0142f30d60df4f96eb0515dc84b8f46d6c
Gihoon Kim <gkim61@myseneca.ca>
2025-09-04 00:31:30 -0400

    Apply portal for the Modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 export default function ResultModal(props) {
   const { ref, targetTime, remainingTime, onReset } = props;
@@ -17,7 +18,7 @@ export default function ResultModal(props) {
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -31,6 +32,7 @@ export default function ResultModal(props) {
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 }
