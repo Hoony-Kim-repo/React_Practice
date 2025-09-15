6876e5b21f1d228e1ad8fe56cadb64abb6cb0f57
Gihoon Kim <gkim61@myseneca.ca>
2025-09-15 03:10:59 -0400

    Add Initial Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.jpg";
+
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="logo" />
+        <h1>React Food Order</h1>
+      </div>
+
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
