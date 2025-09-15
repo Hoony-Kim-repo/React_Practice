f9250ed425dd7444084e0172f0b7039089299721
Gihoon Kim <gkim61@myseneca.ca>
2025-09-15 03:37:34 -0400

    Add MealItem component to show each meal item

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,26 @@
+const MealItem = (prop) => {
+  const { meal } = prop;
+
+  return (
+    <li className="meal-item" key={meal.id}>
+      <article>
+        <img
+          src={`${import.meta.env.VITE_SERVER_URL}/${meal.image}`}
+          alt={meal.name}
+        />
+
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">${meal.price}</p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+
+        <p className="meal-item-actions">
+          <button>Add to Cart</button>
+        </p>
+      </article>
+    </li>
+  );
+};
+
+export default MealItem;
