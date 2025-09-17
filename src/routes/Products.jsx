34bbe2afd9beeac67b491f450a9e0a3552793b8c
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-17 17:29:05 -0400

    Navigating to other page with Link

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <h1>Products Page</h1>{" "}
+      <p>
+        Go to <Link to="/">the list of products</Link>.
+      </p>
+    </>
+  );
 };
 
 export default Products;
