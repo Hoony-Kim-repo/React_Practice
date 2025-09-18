7f14988885cb8781823e596f8d1328aea91c4591
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-18 19:59:07 -0400

    Add Error PAge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./routes/Error";
 import Home from "./routes/Home";
 import Products from "./routes/Products";
 import RootLayout from "./routes/Root";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
+    errorElement: <Error />,
     children: [
       { path: "/", Component: Home },
       { path: "/products", Component: Products },
