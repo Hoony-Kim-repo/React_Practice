eabb2c1ae68fb6b68e12878ad5596740931b3d37
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-18 20:25:06 -0400

    Dynamically add paths

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+  { id: "p1", title: "Product 1" },
+  { id: "p2", title: "Product 2" },
+  { id: "p3", title: "Product 3" },
+];
+
 const Products = () => {
   return (
     <>
@@ -7,6 +13,13 @@ const Products = () => {
       <p>
         Go to <Link to="/">the list of products</Link>.
       </p>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
