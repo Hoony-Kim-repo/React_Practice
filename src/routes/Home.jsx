4e6ec3d04b8151e4143c16192e074eb6183133df
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-18 20:11:35 -0400

    Navigate programmatically

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const onNavigateToProductPage = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>Home Page</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={onNavigateToProductPage}>Products</button>
       </p>
     </>
   );
