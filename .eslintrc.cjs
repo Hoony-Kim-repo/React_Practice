ba8a68134e7e0c39a5a2aebcf0260ba0af3abe62
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-20 23:10:34 -0400

    Initial Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true, // 최신 버전으로 통일
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier", // 마지막에 배치
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: "detect" } },
+  plugins: ["react", "react-hooks", "prettier", "import", "react-refresh"],
+  rules: {
+    "no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "never",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
