93b334ec360abcbd52cc430056cc1dedb0498e4a
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-22 17:05:46 -0400

    Add status on deletion event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,11 +1,15 @@
 import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 import { deleteEvent, fetchEvent } from "../../util/http.js";
 import Header from "../Header.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
+import Modal from "../UI/Modal.jsx";
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const params = useParams();
   const id = params.id;
   const navigate = useNavigate();
@@ -21,7 +25,12 @@ export default function EventDetails() {
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isMutatePending,
+    isError: isMutateError,
+    error: mutateError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
@@ -32,6 +41,14 @@ export default function EventDetails() {
       navigate("..");
     },
   });
+
+  const onStartDelete = () => {
+    setIsDeleting(true);
+  };
+
+  const onStopDelete = () => {
+    setIsDeleting(false);
+  };
 
   const onDelete = () => {
     mutate({ id });
@@ -48,6 +65,38 @@ export default function EventDetails() {
 
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={onStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this event ? This action cannot be
+            undone.
+          </p>
+          <div className="form-actions">
+            {isMutatePending ? (
+              <p>Deleting, Please wait...</p>
+            ) : (
+              <>
+                <button onClick={onStopDelete} className="button-text">
+                  Cacncel
+                </button>
+                <button onClick={onDelete} className="button">
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          {isMutateError && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                mutateError.info?.message ||
+                "Failed to delete event, please try again later"
+              }
+            />
+          )}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
@@ -74,7 +123,7 @@ export default function EventDetails() {
           <header>
             <h1>{queryData.title}</h1>
             <nav>
-              <button onClick={onDelete}>Delete</button>
+              <button onClick={onStartDelete}>Delete</button>
               <Link to="edit">Edit</Link>
             </nav>
           </header>
