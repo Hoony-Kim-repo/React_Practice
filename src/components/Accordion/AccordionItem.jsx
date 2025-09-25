2d683deda271245cb0e20a0eb9a03b4e09f40515
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-24 20:38:27 -0400

    Sharing corss-component state

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,5 +1,24 @@
-const AccordionItem = ({ className, children }) => {
-  return <li className={className}>{children}</li>;
+import { createContext, useContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export const useAccordionItemContext = () => {
+  const context = useContext(AccordionItemContext);
+
+  if (!context)
+    throw new Error(
+      "AccordionItem-related components must be wrapped by <Accordion.Item>."
+    );
+
+  return context;
+};
+
+const AccordionItem = ({ id, className, children }) => {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
 };
 
 export default AccordionItem;
